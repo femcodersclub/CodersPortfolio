a435f66e899de21e4897e82961b1c94986f7583f
Irina Ichim <onlyirina7@gmail.com>
2025-02-04 02:07:59 +0100

    GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/CodersPortfolio/', 
   plugins: [react(), tailwindcss()]
 })
